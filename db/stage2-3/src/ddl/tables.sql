1a2d7c20080fdd07116e76943cb51e12cb68aca9
Alexey Lapin <333893@niuitmo.ru>
2024-02-06 17:48:24 +0300

    readme upd

---
+++ db/stage2-3/src/ddl/tables.sql
@@ -0,0 +1,112 @@
+CREATE TABLE IF NOT EXISTS entities(
+    id              serial            PRIMARY KEY,
+    wikiType        varchar(32)       NOT NULL CHECK ( wikiType in ('POKEMON', 'STONE') )
+);
+
+CREATE TABLE IF NOT EXISTS baseStats(
+    id          serial      PRIMARY KEY,
+    hp          integer     NOT NULL CHECK ( hp >= 0),
+    attack      integer     NOT NULL CHECK ( attack >= 0),
+    defense     integer     NOT NULL CHECK ( defense >= 0),
+    spAttack    integer     NOT NULL CHECK ( spAttack >= 0),
+    spDefense   integer    NOT NULL CHECK ( spDefense >= 0),
+    speed       integer     NOT NULL CHECK ( speed >= 0)
+);
+
+CREATE TABLE IF NOT EXISTS pokemonWiki(
+    id                  serial            PRIMARY KEY,
+    name                varchar(32)       NOT NULL CHECK ( name ~* '^[a-zé\s]+$'),
+    nationalNum         integer           NOT NULL CHECK ( nationalNum >= 1 and nationalNum <= 9999),
+    species             varchar(32)       NOT NULL CHECK ( species ~* '^[a-zé\s]+$'),
+    height              float             NOT NULL CHECK ( height >= 0 ),
+    weight              float             NOT NULL CHECK ( weight >= 0 ),
+    baseStatsID         integer           NOT NULL REFERENCES baseStats(id) ON DELETE CASCADE,
+    pokemonImageLink    text              NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS pokemon(
+    entityID       integer           PRIMARY KEY REFERENCES entities(id) ON DELETE CASCADE,
+    pokemonWikiID  integer           NOT NULL REFERENCES pokemonWiki(id) ON DELETE CASCADE,
+    level          integer           NOT NULL CHECK ( level >= 0)
+);
+
+CREATE TABLE IF NOT EXISTS pokemonTypes(
+    id          serial            PRIMARY KEY,
+    name        varchar(32)       NOT NULL UNIQUE CHECK ( name ~ '^[A-Z]+$')
+);
+
+CREATE TABLE IF NOT EXISTS pokemonToTypes(
+    pokemonID       integer       REFERENCES pokemonWiki(id) ON DELETE CASCADE,
+    pokemonTypesID  integer       REFERENCES pokemonTypes(id) ON DELETE CASCADE,
+    PRIMARY KEY (pokemonID, pokemonTypesID)
+);
+
+CREATE TABLE IF NOT EXISTS stoneWiki(
+    entityID       integer           PRIMARY KEY REFERENCES entities(id) ON DELETE CASCADE,
+    name           varchar(32)       UNIQUE NOT NULL CHECK ( name ~* '^[a-zé\s]+$')
+);
+
+CREATE TABLE IF NOT EXISTS evolutionChains(
+    EvolvingFromID  integer       REFERENCES pokemonWiki(id) ON DELETE CASCADE,
+    EvolvingToID    integer       REFERENCES pokemonWiki(id) ON DELETE CASCADE,
+    stoneID         integer       NOT NULL REFERENCES stoneWiki(entityID)  ON DELETE CASCADE,
+    PRIMARY KEY (EvolvingFromID, EvolvingToID)
+);
+
+
+
+CREATE TABLE IF NOT EXISTS inStock(
+    entityID integer           PRIMARY KEY REFERENCES entities(id) ON DELETE CASCADE,
+    quantity integer           NOT NULL CHECK ( quantity >= 0),
+    price    integer           NOT NULL CHECK ( price >= 0)
+);
+
+CREATE TABLE IF NOT EXISTS gameStylesDist(
+    id                  serial            PRIMARY KEY,
+    physicalSweeper     integer           NOT NULL CHECK ( physicalSweeper >= 0),
+    specialSweeper      integer           NOT NULL CHECK ( specialSweeper >= 0),
+    wall                integer           NOT NULL CHECK ( wall >= 0),
+    physicalTank        integer           NOT NULL CHECK ( physicalTank >= 0),
+    specialTank         integer           NOT NULL CHECK ( specialTank >= 0),
+    CHECK(physicalSweeper + specialSweeper + wall + physicalTank + specialTank = 100)
+);
+
+CREATE TABLE IF NOT EXISTS trainers(
+    id          serial            PRIMARY KEY,
+    firstname   varchar(32)       NOT NULL,
+    lastname    varchar(32)       NOT NULL,
+    gender      varchar(32)       NOT NULL CHECK ( gender in ('MALE', 'FEMALE') ),
+    level       integer           NOT NULL CHECK ( level >= 0),
+    gameStyleID integer           REFERENCES gameStylesDist(id) ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS trainersEntities(
+    trainerID   integer           REFERENCES trainers(id) ON DELETE CASCADE,
+    entityID    integer           REFERENCES entities(id) ON DELETE CASCADE,
+    PRIMARY KEY (trainerID, entityID)
+);
+
+CREATE TABLE IF NOT EXISTS _user(
+    id          serial            PRIMARY KEY,
+    username    varchar(255)      NOT NULL UNIQUE,
+    password    varchar(255)      NOT NULL,
+    avatar      varchar(255),
+    trainerID   integer           REFERENCES trainers(id) ON DELETE CASCADE,
+    role        varchar(255)      check (role in ('USER','ADMIN'))
+);
+
+CREATE TABLE IF NOT EXISTS orders(
+    id          serial           PRIMARY KEY,
+    UserID      integer          NOT NULL REFERENCES _user(id) ON DELETE CASCADE,
+    status    varchar(255)     check (status in ('PROCESSING','DONE','PAID','CANCELLED')),
+    totalPrice  integer          NOT NULL CHECK ( totalPrice >= 0 ) DEFAULT 0,
+    orderDate   timestamp     DEFAULT CURRENT_DATE CHECK (orderDate <= CURRENT_DATE)
+);
+
+CREATE TABLE IF NOT EXISTS orderitems(
+    orderID     integer          NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
+    itemID      integer          NOT NULL REFERENCES entities(id) ON DELETE CASCADE,
+    price       integer          NOT NULL CHECK ( price >= 0),
+    quantity    integer          NOT NULL CHECK ( quantity >= 0),
+    PRIMARY KEY (orderID, itemID)
+);
---
+++ db/stage2-3/src/triggers.sql
@@ -0,0 +1,73 @@
+
+
+-- CREATE OR REPLACE FUNCTION updateTotalPrice() RETURNS TRIGGER AS $$
+-- BEGIN
+--     UPDATE "orders" SET totalprice = (SELECT SUM(instock.price * orderitems.quantity) FROM orderitems
+--                                                 JOIN instock on instock.entityid = orderitems.itemid
+--                                                 WHERE orderitems.orderid = NEW.orderid
+--                                                 GROUP BY orderitems.orderid)
+--                                                 WHERE "orders".id = NEW.orderid;
+--     RETURN NEW;
+-- END
+-- $$
+--     LANGUAGE plpgsql;
+
+
+-- CREATE or REPLACE trigger totalPriceTrigger
+-- after INSERT on orderitems
+-- FOR EACH ROW EXECUTE PROCEDURE updateTotalPrice();
+
+
+-- CREATE OR REPLACE FUNCTION updateTotalItemQuantity() RETURNS TRIGGER AS $$
+-- BEGIN
+--     IF ( (SELECT COUNT(*) FROM orderitems WHERE orderitems.itemid = NEW.itemid and orderitems.orderid = NEW.orderid) >= 1 )
+--         THEN
+--         UPDATE orderitems SET quantity = quantity + NEW.quantity WHERE itemid = NEW.itemid and orderitems.orderid = NEW.orderid;
+--         RETURN NULL;
+--     ELSE
+--         RETURN NEW;
+--     END IF;
+-- END;
+-- $$ LANGUAGE plpgsql;
+
+
+-- CREATE OR REPLACE FUNCTION resetInstoke() RETURNS TRIGGER AS $$
+-- declare mviews record;
+-- BEGIN
+--     IF (new.status = 'PROCESSING')
+--         THEN
+--         for mviews in
+--         select quantity, itemid
+--         from orderitems where orderitems.orderid=new.id
+--         order by 1
+--         loop
+--         perform increaser(mviews.itemid, mviews.quantity);
+--         end loop;
+--         update orderitems set quantity=0 where orderitems.orderid=new.id;
+--     END IF;
+--
+--     RETURN NEW;
+-- END
+-- $$ LANGUAGE plpgsql;
+--
+-- CREATE OR REPLACE TRIGGER resetInstokeTrigger
+-- BEFORE update ON orders
+-- FOR EACH ROW EXECUTE PROCEDURE resetInstoke();
+
+CREATE OR REPLACE FUNCTION set_orderdate_default()
+  RETURNS TRIGGER AS
+$$
+BEGIN
+    IF NEW.orderdate IS NULL THEN
+        NEW.orderdate := CURRENT_DATE;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE or REPLACE TRIGGER trigger_set_orderdate_default
+BEFORE INSERT ON orders
+FOR EACH ROW
+EXECUTE FUNCTION set_orderdate_default();
+
+
